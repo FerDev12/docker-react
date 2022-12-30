3d095df7eaa7f3ca06b7c25f2b1278e5c130749a
FerDev12 <fernando.mu.dev@gmail.com>
2022-12-30 11:16:01 -0600

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# PRODUCTION IMAGE
+
+### Build Phase ###
+FROM node:16-alpine as builder
+WORKDIR /app
+COPY ./package.json .
+RUN npm install
+COPY . .
+RUN npm run build
+
+### Run Phase ###
+FROM nginx
+# Copy build folder from previous phase into /usr/share/nginx/html
+COPY --from=builder /app/build /usr/share/nginx/html
+
+
+
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,12 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/hi there/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/hi there/i);
   expect(linkElement).toBeInTheDocument();
 });
